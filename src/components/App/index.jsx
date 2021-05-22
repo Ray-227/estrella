8a72a430afed0212b305db249c135597faacedb9
Ray <ray-227@yandex.com>
2021-05-22 18:35:37 +0500

    BottomFixedMenu

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,12 +2,14 @@ import React, { Fragment } from 'react';
 import './index.scss';
 
 import TopMenu from '@/components/Blocks/TopMenu/index.jsx';
+import BottomFixedMenu from '@/components/Blocks/BottomFixedMenu/index.jsx';
 
 export default class App extends React.Component {
     render() {
       return (
         <Fragment>
             <TopMenu></TopMenu>
+            <BottomFixedMenu></BottomFixedMenu>
         </Fragment>
       )
     }
